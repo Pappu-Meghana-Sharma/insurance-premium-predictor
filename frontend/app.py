c3fcbd7f6881186ce93bda57e0f0376023dfe3bd
Meghana Sharma <pappumeghana2006@gmail.com>
2025-12-31 01:55:37 +0530

    Changed the URL of frontend to deployed version of api

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
 
-API_URL = "http://backend:8000/predict" 
+# API_URL = "http://backend:8000/predict" 
+API_URL ="https://insurance-premium-predictor-bbzf.onrender.com"
 
 st.title("Insurance Premium Category Predictor")
 st.markdown("Enter your details below:")
